4964a7e89db526f360abc2ccd251df4b9690c0bb
T-800terminator <tomaszczerba123@gmail.com>
2022-10-12 11:36:20 +0200

    tescik1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCKTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>prosze o 2</title>
+</head>
+<body>
+    <?php
+        $con = new mysqli("51.123.43.245", "admin", "123456")
+        
+
+    ?>
+</body>
+
+
+
+
+
